c45f767b04eb063bf449f2dc3f50e08ab5766da8
Nuriddin <ng19990107@gmail.com>
2026-05-29 23:53:04 +0500

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import './training'
+import './training';
+import { Color } from '../enums/color';
+import './collection';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,26 @@ import './training'
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  companyName: string = 'румтибет';
 
+  isPrimaryColor(color: string): boolean {
+    return color === Color.RED || color === Color.GREEN || color === Color.BLUE;
+  }
+
+  constructor() {
+    this.saveLastVisit();
+    this.saveNumberOfVisits();
+    console.log(localStorage.getItem('lastVisitTime'));
+    console.log(localStorage.getItem('NumberOfVisits'));
+  }
+
+  saveLastVisit(): void {
+    const time = Date.now().toString();
+    localStorage.setItem('lastVisitTime', time);
+  }
+
+  saveNumberOfVisits(): void {
+    const numberOfVisitsBefore = Number(localStorage.getItem('NumberOfVisits')) || 0;
+    localStorage.setItem('NumberOfVisits', String(numberOfVisitsBefore + 1));
+  }
 }
